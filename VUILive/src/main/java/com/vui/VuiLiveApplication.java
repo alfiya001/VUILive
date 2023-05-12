5009e0293c05aa2c76042a77747642379ecfe999
2106421 <2106421@LTIN437092.cts.com>
2023-05-12 21:03:05 +0530

    test

--- VUILive/src/main/java/com/vui/VuiLiveApplication.java
+++ VUILive/src/main/java/com/vui/VuiLiveApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VuiLiveApplication {
 
 	public static void main(String[] args) {
+		System.out.println("tyutyty");
 		SpringApplication.run(VuiLiveApplication.class, args);
 	}
 
