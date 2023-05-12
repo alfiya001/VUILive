fbe795b5017271a3d3c72a560c1e9cfa051c412b
2106421 <2106421@LTIN437092.cts.com>
2023-05-12 20:49:27 +0530

    project creation

---
+++ VUILive/src/main/java/com/vui/VuiLiveApplication.java
@@ -0,0 +1,13 @@
+package com.vui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuiLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuiLiveApplication.class, args);
+	}
+
+}
---
+++ VUILive/src/test/java/com/vui/VuiLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuiLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
